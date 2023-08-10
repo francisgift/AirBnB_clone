46f8b34d6c8307639be122f7a9a59cdb9a3d106f
francisgift <81424888+francisgift@users.noreply.github.com>
2023-08-10 18:36:19 +0100

    Create review.py

---
+++ models/review.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''class inherent of BaseModel'''
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    '''class Reviews'''
+
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Review"""
+        super().__init__(*args, **kwargs)
