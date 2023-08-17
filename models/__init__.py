e5d5c45cf85a9543d726af3ed9af36e1830746c5
francisgift <81424888+francisgift@users.noreply.github.com>
2023-08-17 18:47:40 +0100

    Delete models directory

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""create a unique FileStorage instance for your app"""
-from models.engine.file_storage import FileStorage
-
-"""A variable storage,instance of FileStorage"""
-storage = FileStorage()
-storage.reload()
